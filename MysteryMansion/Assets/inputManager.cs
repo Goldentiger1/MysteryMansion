b1cf381b44cbecca41a5b4557a9c637f7c05ed23
OPISTO\oskari.hermunen <ozzah7@hotmail.com>
2018-08-15 13:57:46 +0300

    Rudimentary movement, navmesh

---
+++ MysteryMansion/Assets/inputManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class inputManager : MonoBehaviour {
+
+    public LayerMask ground;
+    public NavMeshAgent player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("ProtoPlayer").GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground)) {
+                player.destination = hit.point;
+            }
+        }
+	}
+}
