ef3cba29068ab900a246a79fd4e4c8c8fe5648c8
OPISTO\oskari.hermunen <ozzah7@hotmail.com>
2018-08-16 13:54:13 +0300

    ..

---
+++ MysteryMansion/Assets/roomSwap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class roomSwap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            SceneManager.LoadScene("Room2");
+        }
+    }
+}
