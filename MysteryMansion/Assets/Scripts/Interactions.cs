72a7350f5d9a0a9de94f34dab23a6b60bb0da683
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-08-17 15:16:32 +0300

    Touch works next pickup

--- MysteryMansion/Assets/Scripts/Interactions.cs
+++ MysteryMansion/Assets/Scripts/Interactions.cs
@@ -1,15 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Interactions : MonoBehaviour {
-
+    public LayerMask itemLM;
+    public Text dbText; // Debug purpose
 
     void Update() {
-        if(Input.touchCount > 0) {
-            foreach(Touch finger in Input.touches) {
-                if(Input.GetTouch(finger.fingerId).phase == TouchPhase.Began) {
+        /*
+        if (Input.touchCount > 0) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            // Osuiko raycast johonkin?
+            dbText.text = "asdlf";
+            Debug.DrawRay(ray.origin, Vector3.forward * 100, Color.red);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
+                hit.collider.gameObject.SetActive(false);
+            }
+        }
+        */
+
+
+            // Onko näytöllä sormia?
+            if (Input.touchCount > 0) {
+            foreach (Touch finger in Input.touches) {
+                // Kun sormi on näytöllä
+                if (Input.GetTouch(finger.fingerId).phase == TouchPhase.Began) {
+
                     Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(finger.fingerId).position);
+                    RaycastHit hit;
+                    // Osuiko raycast johonkin?
+                    if (Physics.Raycast(ray, out hit, Mathf.Infinity, itemLM)) {
+                        //hit.collider.gameObject.SetActive(false);
+                        dbText.text = Input.touchCount.ToString();//debug purpose                                            
+                    }
                 }
             }
         }
