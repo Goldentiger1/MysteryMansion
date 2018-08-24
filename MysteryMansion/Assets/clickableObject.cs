f937a6da3915dff07e08f48549b578dddb0aef14
OPISTO\oskari.hermunen <ozzah7@hotmail.com>
2018-08-24 15:15:48 +0300

    Object inventories rudimentary.

---
+++ MysteryMansion/Assets/clickableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class clickableObject : MonoBehaviour {
+
+    public Canvas canv;
+
+	// Use this for initialization
+	void Start () {
+        canv = GameObject.FindObjectOfType<Canvas>();
+        canv.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseEnter() {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            print("hei, toimii");
+            canv.enabled = true;
+        }
+    }
+}
