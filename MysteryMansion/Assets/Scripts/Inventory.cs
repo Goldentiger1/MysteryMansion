0da618c149735921bda74139189127639774ceb6
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-08-27 15:12:22 +0300

    Inventoria vielä katselen

--- MysteryMansion/Assets/Scripts/Inventory.cs
+++ MysteryMansion/Assets/Scripts/Inventory.cs
@@ -4,26 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour {
-    public int ListSize;
+    public int ListSize = 5;
     public Text dbText; // DEBUG PURPOSE
-    public List<GameObject> ItemsList = new List<GameObject>();
-    public List<Button> InventoryList = new List<Button>();
-
-    void Start() {
-        ItemsList.Capacity = ListSize;
-        InventoryList.Capacity = ListSize;
-    }
-
-    void Update() {
-    }
+    public List<GameObject> ItemsList;
+    public List<Button> InventoryList;
 
     public void Pickup(GameObject item) {
-        for (int i = 0; i < ListSize;) {
-            if (item != null) {
+        for (int i = 0; i < ListSize; i++) {
+            if (item == null) {
                 ItemsList[i] = item;
                 dbText.text = "Item has been added to inventory!!!"; // DEBUG PURPOSE
-                item = null;
-                i++;
+                InventoryList[i].enabled = true;
+                return;
             }
         }
     }
