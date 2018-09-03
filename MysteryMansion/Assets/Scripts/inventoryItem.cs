598b8800acf1148150457a84cd54beff235b3ad9
OPISTO\oskari.hermunen <ozzah7@hotmail.com>
2018-09-03 12:04:18 +0300

    Prefabs for game items.

---
+++ MysteryMansion/Assets/Scripts/inventoryItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventoryItem : MonoBehaviour {
+
+    public Sprite itemSprite;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
