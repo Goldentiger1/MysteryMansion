b7d205aafeeff0a423227bbda4baba81a2ca0495
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-09-03 15:31:52 +0300

    IINNVVEENNTOORR script

--- MysteryMansion/Assets/Scripts/Inventory.cs
+++ MysteryMansion/Assets/Scripts/Inventory.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour {
-    public Text dbText; // DEBUG PURPOSE
+    //public Text dbText; // DEBUG PURPOSE
     public List<GameObject> ItemsList;
     public List<Button> InventoryList;
 
     public void Pickup(GameObject item) {
         ItemsList.Add(item);
-                dbText.text = "BOOP!"; // DEBUG PURPOSE
+        //dbText.text = "BOOP!"; // DEBUG PURPOSE
 
-                return;
+
+
+        //item.GetComponent<ItemScript /*Lisää tähän kerättävän gameobjektin Skripti*/>();
+
+
+        /*
+        itemImages[i].sprite = itemToAdd.sprite;
+        itemImages[i].enabled = true;
+        */
+
+        return;
     }
 }
 
