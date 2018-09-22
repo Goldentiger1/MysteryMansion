c52d89b53f20006a1c9492a568f6bc63e909b1c4
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-09-22 19:20:39 +0300

    Aloitin kartan tekoa

---
+++ MysteryMansion/Assets/Scripts/MapInterface.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapInterface : MonoBehaviour {
+    public GameObject map;
+    public List<Image> roomsImages = new List<Image>();
+
+    void Start() {
+        
+    }
+
+    public void RoomChange(Image room) {
+
+    }
+}
