f16541f3d91cda007c14213109c929497037fa54
OPISTO\oskari.hermunen <ozzah7@hotmail.com>
2018-10-03 13:30:04 +0300

    Final door puzzle pretty much ready, waiting for key and library room.

---
+++ MysteryMansion/Assets/DoorPuzzle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorPuzzle : MonoBehaviour {
+
+    public bool boardsDestroyed = false;
+    public bool lockOpened = false;
+
+    public void Boards() {
+        boardsDestroyed = true;
+    }
+
+    public void Lock() {
+        lockOpened = true;
+    }
+
+    public void Update() {
+        if (boardsDestroyed && lockOpened) {
+            Destroy(GameObject.Find("DoorPuzzler"));
+        }
+    }
+}
--- MysteryMansion/Assets/Scripts/RoomData.cs
+++ MysteryMansion/Assets/Scripts/RoomData.cs
@@ -6,16 +6,17 @@ public class RoomData : MonoBehaviour {
 
     public Camera[] cameras;
 
+
     public void Awake() {
         cameras = GetComponentsInChildren<Camera>();
-    foreach (var cam in cameras) {
+        foreach (var cam in cameras) {
             cam.enabled = false;
         }
     }
 
     public void EnterRoom() {
         print("entered " + gameObject.name);
-    foreach (var cam in cameras) {
+        foreach (var cam in cameras) {
             cam.enabled = true;
         }
     }
@@ -25,5 +26,4 @@ public class RoomData : MonoBehaviour {
             cam.enabled = false;
         }
     }
-
 }
