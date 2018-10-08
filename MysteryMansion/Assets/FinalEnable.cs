1e7d06a0388278c07ab8ef2d808cb2040b512fe1
OPISTO\oskari.hermunen <ozzah7@hotmail.com>
2018-10-08 16:53:25 +0300

    All good but can't build?

---
+++ MysteryMansion/Assets/FinalEnable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalEnable : MonoBehaviour {
+
+    public FinalRoom FR;
+
+    public LayerMask player;
+
+    public void OnTriggerEnter(Collider other) {
+        FR.enabled = true;
+        }
+    }
