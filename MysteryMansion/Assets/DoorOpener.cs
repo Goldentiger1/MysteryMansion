e825796e54e9c46fc5849fe1f9902c0264c46647
OPISTO\oskari.hermunen <ozzah7@hotmail.com>
2018-12-12 12:51:28 +0200

    Added text to point player towards newly opened door in the lobby.

---
+++ MysteryMansion/Assets/DoorOpener.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour {
+
+    public LayerMask Player;
+
+    inputManager IM;
+
+	// Use this for initialization
+	void Start () {
+        IM = FindObjectOfType<inputManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "Protoplayer") {
+            print("osui triggeriin");
+            IM.HapAction();
+            IM.hapText.text = "Wait a minute.. That door on the right, wasn't it closed earlier?";
+        }
+    }
+
+    public void OnTriggerExit(Collider other) {
+        if (other.gameObject.name == "Protoplayer") {
+            print("poistui triggeristä");
+            Destroy(gameObject);
+        }
+    }
+}
